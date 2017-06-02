f2d26938eb7b355468d1c6232d2d479e2b97ba5c
Neel Mehta <neelmehta@college.harvard.edu>
2017-06-02 12:35:58 -0400

    upload deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 # Builds and deploys the app to app.getcabra.com.
-# Run this after running "gulp build".
-# From root of project, run "build/deploy.sh".
+# Run this after running "jekyll build".
+# From root of project, run "./deploy.sh".
+jekyll build
 yes overwrite | duck \
     --upload ftp://hathix5@ftp.hathix.com/public_html _site \
     -assumeyes
