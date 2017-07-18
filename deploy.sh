2ef533d6b633833fb5c0733cd09d940c88b157a4
Neel Mehta <neelmehta@college.harvard.edu>
2017-07-17 22:21:10 -0400

    improve deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # Builds and deploys the app to app.getcabra.com.
 # Run this after running "jekyll build".
 # From root of project, run "./deploy.sh".
-jekyll build
+bundle exec jekyll build
 yes overwrite | duck \
     --upload ftp://hathix5@ftp.hathix.com/public_html _site \
     -assumeyes
